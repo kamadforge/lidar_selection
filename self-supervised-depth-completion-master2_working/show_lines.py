d9b14d93275ebc119c855a31185d2821791e6bf0
Kamil <kamil.m.adamczewski@gmail.com>
2021-01-15 12:39:12 +0100

    getting the visualization of line masks and saving the showing lines

--- self-supervised-depth-completion-master2_working/show_lines.py
+++ self-supervised-depth-completion-master2_working/show_lines.py
@@ -9,6 +9,24 @@ from torch.nn.parameter import Parameter
 torch.set_printoptions(profile="full")
 
 lines=np.load("../kitti_pixels_to_lines.npy")
+
+
+for i in range(65):
+    fig = plt.figure(frameon=False)
+
+    ax = plt.Axes(fig, [0., 0., 1., 1.])
+    ax.set_axis_off()
+    fig.add_axes(ax)
+
+
+    ax.imshow(lines[i], cmap='Greys', aspect='auto')
+    fig.savefig(f'figures/line_{i}.png')
+
+# plt.imshow(lines[35])
+# plt.savefig('figures/line_35.png')
+# plt.show()
+
+
 parameter_mask = torch.tensor(lines)
 
 
@@ -31,7 +49,13 @@ S_mask=torch.max(S_mask_ext, 0)[0]
 S_mask_ind=torch.max(S_mask_ext, 0)[1]
 #print(S_mask_ind[300])
 
+np.set_printoptions(suppress=True)
+point_nums=np.zeros(65,)
 for i in range(len(parameter)):
     print(f"{i}: {len(np.where(S_mask_ind==i)[0])}")
+    point_nums[i] = len(np.where(S_mask_ind==i)[0])
+
+print(np.argsort(point_nums))
+print(np.sort(point_nums))
 
 a=0
