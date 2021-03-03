17869436e0a10542bdb5199cddb8aa00d02edc52
Kamil <kamil.m.adamczewski@gmail.com>
2021-03-03 14:08:34 +0100

    incorporating encoder and decoder to self-supervised. working version of the training from rgb to depth. MAJOR

--- lidar_no_lidar/main_orig_lidnolid.py
+++ lidar_no_lidar/main_orig_lidnolid.py
@@ -6,12 +6,14 @@ import torch
 import torch.nn.parallel
 import torch.optim
 import torch.utils.data
+from torchvision import transforms
 
 from dataloaders.kitti_loader import load_calib, oheight, owidth, input_options, KittiDepth
 #from model import DepthCompletionNet
 from metrics import AverageMeter, Result
 import criteria
 import helper
+import networks
 #from inverse_warp import Intrinsics, homography_from
 
 import matplotlib.pyplot as plt
@@ -20,7 +22,7 @@ import numpy as np
 parser = argparse.ArgumentParser(description='Sparse-to-Dense')
 parser.add_argument('-w',
                     '--workers',
-                    default=4,
+                    default=0, #4
                     type=int,
                     metavar='N',
                     help='number of data loading workers (default: 4)')
@@ -60,7 +62,7 @@ parser.add_argument('--weight-decay',
                     help='weight decay (default: 0)')
 parser.add_argument('--print-freq',
                     '-p',
-                    default=10,
+                    default=100,
                     type=int,
                     metavar='N',
                     help='print frequency (default: 10)')
@@ -77,7 +79,7 @@ parser.add_argument('--data-folder',
 parser.add_argument('-i',
                     '--input',
                     type=str,
-                    default='d', #'gd'
+                    default='rgbd', #'gd'
                     choices=input_options,
                     help='input: | '.join(input_options))
 parser.add_argument('-l',
@@ -107,7 +109,7 @@ parser.add_argument(
     '-m',
     '--train-mode',
     type=str,
-    default="sparse",
+    default="dense",
     choices=["dense", "sparse", "photo", "sparse+photo", "dense+photo"],
     help='dense | sparse | photo | sparse+photo | dense+photo')
 parser.add_argument('-e', '--evaluate', default='', type=str, metavar='PATH')
@@ -141,14 +143,88 @@ depth_criterion = criteria.MaskedMSELoss() if (
 photometric_criterion = criteria.PhotometricLoss()
 smoothness_criterion = criteria.SmoothnessLoss()
 
-if args.use_pose:
-    # hard-coded KITTI camera intrinsics
-    K = load_calib()
-    fu, fv = float(K[0, 0]), float(K[1, 1])
-    cu, cv = float(K[0, 2]), float(K[1, 2])
-    kitti_intrinsics = Intrinsics(owidth, oheight, fu, fv, cu, cv)
-    if cuda:
-        kitti_intrinsics = kitti_intrinsics.cuda()
+
+#
+# #### train
+#
+# def train(self):
+#     """Run the entire training pipeline
+#     """
+#     self.epoch = 0
+#     self.step = 0
+#     self.start_time = time.time()
+#     for self.epoch in range(self.opt.num_epochs):
+#         run_epoch()
+#         if (self.epoch + 1) % self.opt.save_frequency == 0:
+#             self.save_model()
+#
+#
+# def run_epoch(self):
+#     """Run a single epoch of training and validation
+#     """
+#     self.model_lr_scheduler.step()
+#
+#     print("Training")
+#     self.set_train()
+#
+#     for batch_idx, inputs in enumerate(self.train_loader):
+#
+#         before_op_time = time.time()
+#         outputs, losses = self.process_batch(inputs)
+#         self.model_optimizer.zero_grad()
+#         losses["loss"].backward()
+#         self.model_optimizer.step()
+#
+#         duration = time.time() - before_op_time
+#
+#         # log less frequently after the first 2000 steps to save time & disk space
+#         early_phase = batch_idx % self.opt.log_frequency == 0 and self.step < 2000
+#         late_phase = self.step % 2000 == 0
+#
+#         if early_phase or late_phase:
+#             self.log_time(batch_idx, duration, losses["loss"].cpu().data)
+#
+#             if "depth_gt" in inputs:
+#                 self.compute_depth_losses(inputs, outputs, losses)
+#
+#             self.log("train", inputs, outputs, losses)
+#             self.val()
+#
+#         self.step += 1
+#
+#
+# def process_batch(self, inputs):
+#     """Pass a minibatch through the network and generate images and losses
+#     """
+#     for key, ipt in inputs.items():
+#         inputs[key] = ipt.to(self.device)
+#
+#     # Otherwise, we only feed the image with frame_id 0 through the depth encoder
+#     features = self.models["encoder"](inputs["color_aug", 0, 0])
+#     outputs = self.models["depth"](features)
+#
+#     if self.opt.predictive_mask:
+#         outputs["predictive_mask"] = self.models["predictive_mask"](features)
+#
+#     if self.use_pose_net:
+#         outputs.update(self.predict_poses(inputs, features))
+#
+#     self.generate_images_pred(inputs, outputs)
+#     losses = self.compute_losses(inputs, outputs)
+#
+#     return outputs, losses
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def iterate(mode, args, loader, model, optimizer, logger, epoch):
@@ -159,18 +235,17 @@ def iterate(mode, args, loader, model, optimizer, logger, epoch):
     # switch to appropriate mode
     assert mode in ["train", "val", "eval", "test_prediction", "test_completion"], \
         "unsupported mode: {}".format(mode)
+
+    encoder = model[0]; decoder = model[1]
     if mode == 'train':
-        model.train()
+        encoder.train(); decoder.train()
         lr = helper.adjust_learning_rate(args.lr, optimizer, epoch)
     else:
-        model.eval()
+        encoder.train(); decoder.train()
         lr = 0
 
     torch.set_printoptions(profile="full")
     for i, batch_data in enumerate(loader):
-
-
-
 
         start = time.time()
         batch_data = {
@@ -181,8 +256,27 @@ def iterate(mode, args, loader, model, optimizer, logger, epoch):
             'gt'] if mode != 'test_prediction' and mode != 'test_completion' else None
         data_time = time.time() - start
 
+
         start = time.time()
-        pred = model(batch_data)
+
+        # torchvision.transforms.Resize
+        # img = transform.resize(batch_data['rgb'], (192, 640))
+
+        # transform = transforms.Compose([
+        #     transforms.Resize((round(192), round(640))),
+        #     # interpolation `BILINEAR` is applied by default
+        #     transforms.ToTensor(),
+        #     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        # ])
+        # image = transform(batch_data['rgb'])
+
+
+
+        # Otherwise, we only feed the image with frame_id 0 through the depth encoder
+        features = encoder(batch_data['rgb'])
+        outputs = decoder(features)
+
+        # pred = model(batch_data)
         # im = batch_data['d'].detach().cpu().numpy()
         # im_sq = im.squeeze()
         # plt.figure()
@@ -190,7 +284,7 @@ def iterate(mode, args, loader, model, optimizer, logger, epoch):
         # plt.show()
         # for i in range(im_sq.shape[0]):
         #     print(f"{i} - {np.sum(im_sq[i])}")
-
+        pred = outputs[('disp', 0)]
         depth_loss, photometric_loss, smooth_loss, mask = 0, 0, 0, None
         if mode == 'train':
             # Loss 1: the direct depth supervision from ground truth label
@@ -202,46 +296,50 @@ def iterate(mode, args, loader, model, optimizer, logger, epoch):
                 depth_loss = depth_criterion(pred, gt)
                 mask = (gt < 1e-3).float()
 
-            # Loss 2: the self-supervised photometric loss
-            if args.use_pose:
-                # create multi-scale pyramids
-                pred_array = helper.multiscale(pred)
-                rgb_curr_array = helper.multiscale(batch_data['rgb'])
-                rgb_near_array = helper.multiscale(batch_data['rgb_near'])
-                if mask is not None:
-                    mask_array = helper.multiscale(mask)
-                num_scales = len(pred_array)
+            # # Loss 2: the self-supervised photometric loss
+            # if args.use_pose:
+            #     # create multi-scale pyramids
+            #     pred_array = helper.multiscale(pred)
+            #     rgb_curr_array = helper.multiscale(batch_data['rgb'])
+            #     rgb_near_array = helper.multiscale(batch_data['rgb_near'])
+            #     if mask is not None:
+            #         mask_array = helper.multiscale(mask)
+            #     num_scales = len(pred_array)
+            #
+            #     # compute photometric loss at multiple scales
+            #     for scale in range(len(pred_array)):
+            #         pred_ = pred_array[scale]
+            #         rgb_curr_ = rgb_curr_array[scale]
+            #         rgb_near_ = rgb_near_array[scale]
+            #         mask_ = None
+            #         if mask is not None:
+            #             mask_ = mask_array[scale]
+            #
+            #         # compute the corresponding intrinsic parameters
+            #         height_, width_ = pred_.size(2), pred_.size(3)
+            #         intrinsics_ = kitti_intrinsics.scale(height_, width_)
+            #
+            #         # inverse warp from a nearby frame to the current frame
+            #         warped_ = homography_from(rgb_near_, pred_,
+            #                                   batch_data['r_mat'],
+            #                                   batch_data['t_vec'], intrinsics_)
+            #         photometric_loss += photometric_criterion(
+            #             rgb_curr_, warped_, mask_) * (2**(scale - num_scales))
 
-                # compute photometric loss at multiple scales
-                for scale in range(len(pred_array)):
-                    pred_ = pred_array[scale]
-                    rgb_curr_ = rgb_curr_array[scale]
-                    rgb_near_ = rgb_near_array[scale]
-                    mask_ = None
-                    if mask is not None:
-                        mask_ = mask_array[scale]
-
-                    # compute the corresponding intrinsic parameters
-                    height_, width_ = pred_.size(2), pred_.size(3)
-                    intrinsics_ = kitti_intrinsics.scale(height_, width_)
-
-                    # inverse warp from a nearby frame to the current frame
-                    warped_ = homography_from(rgb_near_, pred_,
-                                              batch_data['r_mat'],
-                                              batch_data['t_vec'], intrinsics_)
-                    photometric_loss += photometric_criterion(
-                        rgb_curr_, warped_, mask_) * (2**(scale - num_scales))
-
-            # Loss 3: the depth smoothness loss
-            smooth_loss = smoothness_criterion(pred) if args.w2 > 0 else 0
+            # # Loss 3: the depth smoothness loss
+            # smooth_loss = smoothness_criterion(pred) if args.w2 > 0 else 0
 
             # backprop
-            loss = depth_loss + args.w1 * photometric_loss + args.w2 * smooth_loss
+            #loss = depth_loss + args.w1 * photometric_loss + args.w2 * smooth_loss
+            loss = depth_loss
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
         gpu_time = time.time() - start
+
+        # if i % 9 == 0:
+        #     print(i)
 
         # measure accuracy and record loss
         with torch.no_grad():
@@ -268,13 +366,13 @@ def iterate(mode, args, loader, model, optimizer, logger, epoch):
                 logger.save_img_comparison_as_best(mode, epoch)
             logger.conditional_summarize(mode, avg, is_best)
 
-            helper.save_checkpoint({  # save checkpoint
-                'epoch': epoch,
-                'model': model.module.state_dict(),
-                'best_result': logger.best_result,
-                'optimizer': optimizer.state_dict(),
-                'args': args,
-            }, is_best, epoch, logger.output_directory)
+            # helper.save_checkpoint({  # save checkpoint
+            #     'epoch': epoch,
+            #     'model': model.module.state_dict(),
+            #     'best_result': logger.best_result,
+            #     'optimizer': optimizer.state_dict(),
+            #     'args': args,
+            # }, is_best, epoch, logger.output_directory)
 
     return avg, is_best
 
@@ -312,21 +410,37 @@ def main():
             print("No checkpoint found at '{}'".format(args.resume))
             return
 
-    # print("=> creating model and optimizer ... ", end='')
-    # # model = DepthCompletionNet(args).to(device)
-    # # model_named_params = [
-    # #     p for _, p in model.named_parameters() if p.requires_grad
-    # # ]
-    # # optimizer = torch.optim.Adam(model_named_params,
-    # #                              lr=args.lr,
-    # #                              weight_decay=args.weight_decay)
-    # print("completed.")
+    ################# model
+
+    print("=> creating model and optimizer ... ", end='')
+    ############# MODEL
+
+    parameters_to_train = []
+    encoder = networks.ResnetEncoder(num_layers=18)
+    encoder.to(device)
+    parameters_to_train += list(encoder.parameters())
+
+    decoder = networks.DepthDecoder(encoder.num_ch_enc)
+    decoder.to(device)
+    parameters_to_train += list(decoder.parameters())
+    ##############
+
+    # encoder_named_params = [
+    #     p for _, p in encoder.named_parameters() if p.requires_grad
+    # ]
+    optimizer = torch.optim.Adam(parameters_to_train,
+                                 lr=args.lr,
+                                 weight_decay=args.weight_decay)
+
+
+    print("completed.")
     # if checkpoint is not None:
     #     model.load_state_dict(checkpoint['model'])
     #     optimizer.load_state_dict(checkpoint['optimizer'])
     #     print("=> checkpoint state loaded.")
 
-    # model = torch.nn.DataParallel(model)
+    encoder = torch.nn.DataParallel(encoder)
+    decoder = torch.nn.DataParallel(decoder)
 
     # Data loading code
     print("=> creating data loaders ... ")
@@ -348,12 +462,15 @@ def main():
         pin_memory=True)  # set batch size to be 1 for validation
     print("\t==> val_loader size:{}".format(len(val_loader)))
 
+    ##############################################################
+
     # create backups and results folder
     logger = helper.logger(args)
     # if checkpoint is not None:
     #     logger.best_result = checkpoint['best_result']
     print("=> logger created.")
 
+    model = [encoder, decoder]
     if is_eval:
         print("=> starting model evaluation ...")
         result, is_best = iterate("val", args, val_loader, model, None, logger,
