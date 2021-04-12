619e4c33216faec292027667f06a89651af32284
Kamil <kamil.m.adamczewski@gmail.com>
2021-04-12 12:43:42 +0200

    cleaning

--- self-supervised-depth-completion-master2_working/depth_manipulation.py
+++ self-supervised-depth-completion-master2_working/depth_manipulation.py
@@ -1,17 +1,17 @@
+# This function is used to create a mask which can be used for evaluating the model on the subset of features
+
+
 import numpy as np
 
 from scipy.stats import binned_statistic_2d
 
-
-
-
-
+from depth_draw import draw
 
 # print("bins shape", bins_2d_depth.statistic.shape)
 
 
 #for all the points in the bin we want to change for a fixed set of points so that each bin has the number equally spaced points
-def depth_adjustment(depth, adjust):
+def depth_adjustment(depth, adjust, rgb=None):
     #depth_points[0] - ver coordinates of posiitve dept points
     # depth_points[1] - hor coordinates of posiitve dept points
 
@@ -20,21 +20,26 @@ def depth_adjustment(depth, adjust):
 
     # binning
     size_of_bin = 40
-    # print(depth.shape)
-
     oheight = depth.shape[0]
     owidth = depth.shape[1]
-
     bin_ver = np.arange(0, oheight, size_of_bin)
     bin_ver = np.append(bin_ver, oheight)
     bin_hor = np.arange(0, owidth, size_of_bin)
     bin_hor = np.append(bin_hor, owidth)
     values = depth[np.where(depth > 0)[0], np.where(depth > 0)[1]]  # look at depth values for pixels with non-0 depth
+    square_num = (len(bin_ver)-1)*(len(bin_hor)-1)
+
     # bin function
     bins_2d_depth = binned_statistic_2d(depth_points[0], depth_points[1], values.squeeze(), 'count',
                                         bins=[bin_ver, bin_hor], range=[[0, owidth], [0, oheight]])
+    # hash bin function each bin number has a list of points that belong to that bin
+    bin_hash = {}
+    for b in range(square_num):
+        bin_hash[b] = np.where(bins_2d_depth.binnumber == b)
 
 
+
+    # counting points
     A_this = bins_2d_depth.statistic
     if 'A' not in globals():
         global A;
@@ -58,9 +63,39 @@ def depth_adjustment(depth, adjust):
     print(A_2d_argsort[-10:])
     print("num of points: ", A_2d_sort[-10:])
 
-    #### 
+    # choose ranks for the squares
+    select_mask=True # to create a mask with 1s for selected squates and 0 otherwise
+    squares = np.arange(square_num)
+    sq_mode = "switch"
+    if sq_mode == "random":
+        np.random.seed(15)
+        squares = np.random.choice(square_num, 10)
+    elif sq_mode == "most":
+        squares = np.array([int(a) for a in A_2d_argsort[-10:]])
+    elif sq_mode == "switch":
+        squares = np.load(f"ranks/switches_argsort_2D_iter_507.npy")[-10:]
+    print(f"Squares used {sq_mode}: ", squares)
 
-    ###
+    # draw the selected squares
+    if rgb != None and 0:
+        draw("sq", rgb, squares, A.shape[1])
+
+    ver = np.floor(squares // A.shape[1])
+    hor = squares % A.shape[1]
+    if select_mask:
+        mask_new = np.zeros_like(depth)
+        for p, item in enumerate(squares):
+            verp_scaled= int(ver[p])*size_of_bin
+            horp_scaled=hor[p]*size_of_bin
+            mask_new[verp_scaled:(verp_scaled+size_of_bin), horp_scaled:(horp_scaled+size_of_bin)]=1
+
+        depth=mask_new*depth
+
+
+    remove_depth=0
+    if remove_depth:
+        #depth = np.random.normal(10, 5, (depth.shape[0], depth.shape[1])) #RMSE=21186.322, MAE=19962.695
+        depth =np.zeros_like(depth)
 
     points_wanted_num = 50
     points_choice = "random"
@@ -129,7 +164,10 @@ def depth_adjustment(depth, adjust):
             #print(len(np.where(depth_new>0)[0]))
 
         #print(table_i)
-        return depth_new, table_i
+
+        depth = depth_new
+
+    return depth
 
 
 def depth_adjustment_lines(depth):
