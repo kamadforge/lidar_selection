3aef95ef8a08cad01201131cda528bd9eeae9a19
Kamil Adamczewski <kadamczewski@login2.is.localnet>
2021-05-21 21:34:56 +0200

    minor

--- self-supervised-depth-completion-master2_working/parameter_search.py
+++ self-supervised-depth-completion-master2_working/parameter_search.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
-
+import sys
+print(sys.version)
 
 
 for lr in [0.0001, 1e-5]:
@@ -16,11 +17,11 @@ for lr in [0.0001, 1e-5]:
             folder_and_name = path_name.split(os.sep)[-2:]
 
 
-            subprocess.call(["python", "main_sw.py",
+            subprocess.call(["python", "main_sw.py", "--workers", "4",
             "--data-folder",  "/home/kamil/Dropbox/Current_research/data/kitti",
             "-e", path_name])
 
 
-            subprocess.call(["python", "main_orig.py",
+            subprocess.call(["python", "main_orig.py", "--workers", "4",
             "--data-folder",  "/home/kamil/Dropbox/Current_research/data/kitti",
             "-e", "/home/kamil/Dropbox/Current_research/depth_completion_opt/results/good/mode=dense.input=gd.resnet34.criterion=l2.lr=1e-05.bs=1.wd=0.pretrained=False.jitter=0.1.time=2021-04-01@19-36/checkpoint--1_i_16600_typefeature_None.pth.tar", "--ranks_file"] + folder_and_name)
