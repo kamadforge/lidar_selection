0117bb44a61c1c832e8840ea1b1f19351b9d63b2
Kamil <kamil.m.adamczewski@gmail.com>
2021-07-09 21:21:33 +0200

    random seed fix

--- self-supervised-depth-completion-master2_working/main_orig.py
+++ self-supervised-depth-completion-master2_working/main_orig.py
@@ -123,11 +123,12 @@ parser.add_argument('-e', '--evaluate', default='', type=str, metavar='PATH')
 # parser.add_argument('-e', '--evaluate', default="/home/kamil/Dropbox/Current_research/depth_completion_opt/results/good/mode=dense.input=d.resnet34.criterion=l2.lr=1e-05.bs=1.wd=0.pretrained=False.jitter=0.1.time=2021-05-03@21-17/checkpoint--1_i_85850_typefeature_None.pth.tar")
 
 parser.add_argument('--cpu', action="store_true", help='run on cpu')
-parser.add_argument('--type_feature', default="lines", choices=["sq", "lines", "None"])
+parser.add_argument('--type_feature', default="sq", choices=["sq", "lines", "None"])
 parser.add_argument('--depth_adjust', default=1, type=int)
 parser.add_argument('--sparse_depth_source', default='nonbin')
 #parser.add_argument('--ranks_file', nargs="+", default=["la", "la"])
 parser.add_argument('--ranks_file', default="/home/kamil/Dropbox/Current_research/depth_completion_opt/self-supervised-depth-completion-master2_working/ranks/lines/global/16600_switches_2D_equal_iter_3990.npy")
+parser.add_argument('--seed', type=int)
 args = parser.parse_args()
 
 if args.evaluate == "1":
@@ -345,11 +346,11 @@ def iterate(mode, args, loader, model, optimizer, logger, epoch):
         if depth_adjust and args.use_d and args.evaluate:
             if args.type_feature == "sq":
                 if args.use_rgb:
-                    depth_new = depth_adjustment(batch_data['d'], adjust_features, i, args.ranks_file, batch_data['rgb'])
+                    depth_new = depth_adjustment(batch_data['d'], adjust_features, i, args.ranks_file, args.seed, batch_data['rgb'])
                 else:
-                    depth_new = depth_adjustment(batch_data['d'], adjust_features, i, args.ranks_file)
+                    depth_new = depth_adjustment(batch_data['d'], adjust_features, i, args.ranks_file, args.seed)
             elif args.type_feature == "lines":
-                depth_new = depth_adjustment_lines(batch_data['d'], adjust_features, i, args.ranks_file)
+                depth_new = depth_adjustment_lines(batch_data['d'], adjust_features, i, args.ranks_file, args.seed)
 
             batch_data['d'] = torch.Tensor(depth_new).unsqueeze(0).unsqueeze(1).to(device)
         data_time = time.time() - start
