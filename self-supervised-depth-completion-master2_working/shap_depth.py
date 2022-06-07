91bf36e4da62e204fc3ee9bfecdc52ff20a5ff57
Kamil <kamil.m.adamczewski@gmail.com>
2022-06-07 16:51:58 +0200

    computing shap locally for every file

--- self-supervised-depth-completion-master2_working/shap_depth.py
+++ self-supervised-depth-completion-master2_working/shap_depth.py
@@ -1,4 +1,5 @@
 # it's a file that is similar to the one from publish_dirichlet and shapley_rank.py
+import os
 
 from sklearn.linear_model import LinearRegression
 import numpy as np
@@ -8,51 +9,64 @@ filename = "ranks/lines/global/shap/lines_shap.txt"
 filename = "/home/kamil/Dropbox/Current_research/depth_completion_opt/self-supervised-depth-completion-master2_working/ranks/lines/global/shap/lines_shap.txt"
 filename_clean = "/home/kamil/Dropbox/Current_research/depth_completion_opt/self-supervised-depth-completion-master2_working/ranks/lines/global/shap/lines_shap2.txt"
 
+path_list = "/home/kamil/Dropbox/Current_research/depth_completion_opt/self-supervised-depth-completion-master2_working/ranks/lines/instance/shap/checkpoint_10_i_85000__best.pth.tar/"
+file_list = os.listdir(path_list)
 
-#remove the multiple entries
-file = open(filename)
-s = {}
-for line in file:
-    split_keyval = line.strip().split(":")
-    if len(split_keyval[0])>0: #0 subset
-        s[split_keyval[0]]=split_keyval[1]
+shapimp_dict={}
+filename_clean = path_list + "temp.txt"# temp
+for filename in file_list:
+    if ".txt" in filename:
+        filepath = path_list + filename
+        print("\n"+filename)
 
-# make a new file with unrepeated entries
-file = open(filename_clean, "w+")
-file.truncate()
-for el in s:
-    file.write(el+":"+s[el]+"\n")
-file = open(filename_clean)
+        #remove the multiple entries
+        file = open(filepath)
+        next(file)
+        s = {}
+        for line in file:
+            split_keyval = line.strip().split(":")
+            if len(split_keyval[0])>0: #0 subset
+                s[split_keyval[0]]=split_keyval[1]
 
-# get the dictionary with key - the nodes in a subset, value: set value (the difference)
-dic = {}
-feat_num=65
-NOPRUNING_VAL = 11315
-for line in file:
-    print(line)
-    split_keyval = line.strip().split(":")
-    val = float(split_keyval[1])
-    key = [int(v) for v in split_keyval[0].split(",")]
-    key_bin = np.zeros(feat_num)
-    key_bin[key]=1
-    dic[tuple(key_bin)]=NOPRUNING_VAL - val
-    m=s
+        # make a new file with unrepeated entries
+        file = open(filename_clean, "w+")
+        file.truncate()
+        for el in s:
+            file.write(el+":"+s[el]+"\n")
+        file = open(filename_clean)
 
-#dic, nodes_num = readdata_notsampled(file_old, acc)
+        # get the dictionary with key - the nodes in a subset, value: set value (the difference)
+        dic = {}
+        feat_num=65
+        NOPRUNING_VAL = 11315
+        for line in file:
+            #print(line)
+            split_keyval = line.strip().split(":")
+            val = float(split_keyval[1])
+            key = [int(v) for v in split_keyval[0].split(",")]
+            key_bin = np.zeros(feat_num)
+            key_bin[key]=1
+            dic[tuple(key_bin)]=NOPRUNING_VAL - val
+            m=s
 
-def nCr(n,k):
-    f = math.factorial
-    return f(n) / (f(k) * f(n-k))
+        #dic, nodes_num = readdata_notsampled(file_old, acc)
 
-# compute the weights from the shapley regression, there is a weight for each data point
-weights=[]
-for i in dic.keys():
-    k = np.sum(i)
-    val = (feat_num-1)/(nCr(feat_num, k)*k*(feat_num-k))
-    weights.append(val)
+        def nCr(n,k):
+            f = math.factorial
+            return f(n) / (f(k) * f(n-k))
 
-reg = LinearRegression().fit(list(dic.keys()), list(dic.values()), weights)
-shap_arr = reg.coef_
-print("shaps\n", shap_arr)
-shap_arr_sorted = np.argsort(shap_arr)[-32:]
-print(",".join([str(a) for a in shap_arr_sorted]))
+        # compute the weights from the shapley regression, there is a weight for each data point
+        weights=[]
+        for i in dic.keys():
+            k = np.sum(i)
+            val = (feat_num-1)/(nCr(feat_num, k)*k*(feat_num-k))
+            weights.append(val)
+
+        reg = LinearRegression().fit(list(dic.keys()), list(dic.values()), weights)
+        shap_arr = reg.coef_
+        #print("shaps\n", shap_arr)
+        shap_arr_sorted = np.argsort(shap_arr)
+        shapimp_dict[filename[:-4]]=shap_arr_sorted
+        print(",".join([str(a) for a in shap_arr_sorted[-32:]]))
+
+np.save(path_list+"shapimp_dict.npy", shapimp_dict)
